26b500adc369c2d895c60f765cc59439d9cd1c5c
Justin Duke <dukerson@gmail.com>
2015-09-20 21:45:45 -0700

    print

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 import twilio.twiml
 
 
@@ -9,7 +9,8 @@ app = Flask(__name__)
 def hello_monkey():
     """Respond to incoming calls with a simple text message."""
 
-    resp = twilio.twiml.Response()
+    print request.values.get("Body")
+
     resp = twilio.twiml.Response()
     with resp.message("") as m:
         m.media("https://s-media-cache-ak0.pinimg.com/736x/ef/94/21/ef9421da2b6da030dca07a3a5fa48107.jpg")
