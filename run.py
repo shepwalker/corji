d145b9067eeb5f983c5cf4c8a5c07c8d28f8904d
Justin Duke <dukerson@gmail.com>
2015-09-22 10:04:20 -0700

    cleanup code, barely

--- run.py
+++ run.py
@@ -6,14 +6,16 @@ import twilio.twiml
 
 app = Flask(__name__)
 
-sheetsu_url = "https://spreadsheets.google.com/feeds/list/1vDkS3vwXrT4mSyI8JVHQ_Z7GGRF90GnUTbX8p0zoqNM/od6/public/values?alt=json"
-payload = requests.get(sheetsu_url).json()
+# TODO: make composable.
+# TODO: move URL to .env variable
+url = "https://spreadsheets.google.com/feeds/list/1vDkS3vwXrT4mSyI8JVHQ_Z7GGRF90GnUTbX8p0zoqNM/od6/public/values?alt=json"
+payload = requests.get(url).json()
 raw_data = payload['feed']['entry']
 corgis = {i['gsx$emoji']['$t']: i['gsx$url']['$t'] for i in raw_data}
 
 
 @app.route("/", methods=['GET', 'POST'])
-def hello_monkey():
+def corgi():
     """Respond to incoming calls with a simple text message."""
 
     emoji = request.values.get("Body") or ""
@@ -35,6 +37,3 @@ def hello_monkey():
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
-# alt ice cream http://www.google.com/imgres?imgurl=http://happyfeminist.typepad.com/happyfeminist/images/img_1244_1.jpg&imgrefurl=http://happyfeminist.typepad.com/happyfeminist/corgis_and_collies_and_scotties_oh_my/&h=450&w=600&tbnid=w8bKiJRn79Bz-M:&docid=QbmFgEsH-ih0FM&ei=16D_VcTaD4LRoATumr-IDA&tbm=isch&ved=0CDUQMygPMA9qFQoTCISi8p-9h8gCFYIoiAodbs0PwQ
