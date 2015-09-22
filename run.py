9937047f0b0db5b94fdad8c085ac7977ecb85fec
Justin Duke <dukerson@gmail.com>
2015-09-22 09:57:29 -0700

    lava

--- run.py
+++ run.py
@@ -6,7 +6,10 @@ import twilio.twiml
 
 app = Flask(__name__)
 
-corgis = {}
+sheetsu_url = "https://spreadsheets.google.com/feeds/list/1vDkS3vwXrT4mSyI8JVHQ_Z7GGRF90GnUTbX8p0zoqNM/od6/public/values?alt=json"
+payload = requests.get(sheetsu_url).json()
+raw_data = payload['feed']['entry']
+corgis = {i['gsx$emoji']['$t']: i['gsx$url']['$t'] for i in raw_data}
 
 
 @app.route("/", methods=['GET', 'POST'])
@@ -31,12 +34,6 @@ def hello_monkey():
 
 
 if __name__ == "__main__":
-    global corgis
-
-    sheetsu_url = "https://spreadsheets.google.com/feeds/list/1vDkS3vwXrT4mSyI8JVHQ_Z7GGRF90GnUTbX8p0zoqNM/od6/public/values?alt=json"
-    payload = requests.get(sheetsu_url).json()
-    raw_data = payload['feed']['entry']
-    corgis = {i['gsx$emoji']['$t']: i['gsx$url']['$t'] for i in raw_data}
     app.run(debug=True)
 
 
