718ee1ae4186a8be45ae5bdef1a116048db80e89
Shepherd Walker <shep.walker@gmail.com>
2015-09-23 20:43:22 -0700

    functioning local cache

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 # This Python file uses the following encoding: utf-8
 
-from flask import Flask, request
+from flask import Flask, request, send_from_directory, abort
 import requests
 import twilio.twiml
 import os
@@ -16,28 +16,40 @@ payload = requests.get(url).json()
 raw_data = payload['feed']['entry']
 corgis = {i['gsx$emoji']['$t']: i['gsx$url']['$t'] for i in raw_data}
 
-cacheDir = os.getenv('CORJI_CACHE_PATH', './cache')
+cache_dir = os.getenv('CORJI_CACHE_PATH', './cache')
+
 for i in corgis:
     corgi = corgis.get(i, None)
     if not corgi:
         continue
 
-    #emojiDir = str(i.encode('unicode_escape')).replace("\\", "").replace("'", "")
-    emojiDir = emoji.demojize(i).replace(":","")
+    emoji_dir = emoji.demojize(i).replace(":", "")
     try:
-        print(emojiDir)
+        print(emoji_dir)
     except:
-        #TODO: insert debugging logic so we know where we're failing to successfully demojize
+        # TODO: insert debugging logic so we know where we're failing to
+        # successfully demojize
         continue
 
-    directory = cacheDir + '/' + emojiDir
+    directory = cache_dir + '/' + emoji_dir
     if not os.path.exists(directory):
         os.makedirs(directory)
         try:
-            urllib.request.urlretrieve(corgi, directory +"/01.jpg")
+            urllib.request.urlretrieve(corgi, directory + "/01.jpg")
         except:
-            print("FAILED ON:" + emojiDir)
-    
+            print("FAILED ON:" + emoji_dir)
+
+
+@app.route("/emoji/<path:file_name>", methods=['GET'])
+def get_image(file_name):
+    print(file_name)
+    full_file_name = cache_dir+"/"+file_name
+    split_name = file_name.split('/')
+    if(os.path.exists(full_file_name)):
+        return send_from_directory(cache_dir + "/"+split_name[0], split_name[1])
+    else:
+        abort(404)
+
 
 @app.route("/", methods=['GET', 'POST'])
 def corgi():
@@ -53,7 +65,12 @@ def corgi():
     if not corgi:
         message = "No corgi :("
 
-    resp = twilio.twiml.Response()
+    possible_corji_dir = emoji_dir + "/" + emoji.demojize(i).replace(":", "")
+
+    if(os.path.exists(possible_corji_dir + "/01.jpg"))
+        corgi = os.path.exists(possible_corji_dir + "/01.jpg"
+
+    resp=twilio.twiml.Response()
     with resp.message(message) as m:
         if corgi:
             m.media(corgi)
