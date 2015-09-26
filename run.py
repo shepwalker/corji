cfe5127600e0d591d1883c99cba65e90e1c178eb
Shepherd Walker <shep.walker@gmail.com>
2015-09-26 16:48:18 -0700

    Initial logging

--- run.py
+++ run.py
@@ -1,6 +1,12 @@
 # This Python file uses the following encoding: utf-8
 import os
+import sys
+import logging
 
+from logging.handlers import (
+    TimedRotatingFileHandler
+)
+from functools import wraps
 from flask import (
     Flask,
     send_from_directory,
@@ -15,12 +21,39 @@ from exceptions import CorgiNotFoundException
 
 app = Flask(__name__)
 
-SPREADSHEET_URL = os.getenv('CORGI_URL', '')
+# logging nonsense
+logpath = os.getenv('CORJI_LOG_PATH', './logs')
 
+file_handler = TimedRotatingFileHandler(logpath + '/corji.log', 'midnight', 1)
+file_handler.setFormatter(
+    logging.Formatter('%(asctime)s %(name)-12s %(levelname)-8s %(message)s'))
+file_handler.setLevel(logging.INFO)
+mainLogger = app.logger
+mainLogger.addHandler(file_handler)
+mainLogger.setLevel(logging.INFO)
+
+if app.debug:
+    local_handler = logging.StreamHandler(sys.stdout)
+    mainLogger.addHandler(local_handler)
+
+SPREADSHEET_URL = os.getenv('CORGI_URL', '')
+mainLogger.debug("Spreadsheet URL defined: %s", SPREADSHEET_URL)
 # TODO: GLOBALS BAD.
 corgis = data_sources.load_from_spreadsheet(SPREADSHEET_URL)
-cache.put_in_local_cache(corgis)
 
+mainLogger.debug("Starting to load Corjis into cache.")
+cache.put_in_local_cache(corgis)
+mainLogger.debug("Completed Corji Cache loading")
+
+
+def logged_view(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        mainLogger.info("Request to Corji: %s",args)
+        fn = f(*args, **kwargs)
+        mainLogger.info("Response to Corji: %s", fn)
+        return fn
+    return decorated_function
 
 # TODO: Serve statics not via Flask.
 @app.route("/local/<path:file_name>", methods=['GET'])
@@ -41,9 +74,11 @@ def get_corgi(emoji):
     try:
         possible_corji_path = cache.get_from_local_cache(emoji)
     except CorgiNotFoundException as e:
+        mainLogger.warn("Corji not found for request %s", emoji)
         return str(resp.message(e.message()))
 
-    corgi = request.url_root + url_for('get_image', file_name=possible_corji_path)
+    corgi = request.url_root + \
+        url_for('get_image', file_name=possible_corji_path)
     with resp.message() as m:
         m.media(corgi)
 
@@ -51,9 +86,9 @@ def get_corgi(emoji):
 
 
 @app.route("/", methods=['GET', 'POST'])
+@logged_view
 def corgi():
     """Respond to incoming calls with a simple text message."""
-
     this_emoji = request.values.get("Body") or ""
     return get_corgi(this_emoji)
 
