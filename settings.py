67ff75923851eab14f3b72841c4e2b17c6b733ee
Justin Duke <me@jmduke.com>
2015-09-28 15:33:01 -0700

    DEBUG to env variable.

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 import os
 
 class Config(object):
-	DEBUG = True
+	DEBUG = os.getenv('DEBUG', False)
 	CORJI_LOG_PATH = os.getenv('CORJI_LOG_PATH', './logs')
 	CORJI_LOG_NAME = os.getenv('CORJI_LOG_NAME', 'corji.log')
 	CORJI_SPREADSHEET_URL = os.getenv('CORGI_URL', '')
