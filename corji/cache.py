8c125c8528ac48b6d9ac798abc08a24624848b69
Shepherd Walker <shep.walker@gmail.com>
2015-10-08 16:33:52 -0700

    Nuked local cache because we weren't using it and it wasn't valuable

--- corji/cache.py
+++ corji/cache.py
@@ -21,33 +21,6 @@ if Config.REMOTE_CACHE_POPULATE_ENABLED or
         Bucket=Config.AWS_S3_CACHE_BUCKET_NAME)
 
 
-def put_in_local_cache(corgis):
-    for i in corgis:
-        corgi = corgis.get(i, None)
-        if not corgi:
-            continue
-
-        emoji_dir = emoji.demojize(i).replace(":", "")
-        try:
-            directory = Config.CACHE_DIR + '/' + emoji_dir
-            if not os.path.exists(directory):
-                os.makedirs(directory)
-                urllib.request.urlretrieve(corgi, directory + "/01.jpg")
-        except:
-            logger.error("Failed on: " + i)
-
-
-def get_from_local_cache(raw_emoji):
-    filename = emoji.demojize(raw_emoji).replace(":", "")
-    cached_filename = Config.CACHE_DIR + "/" + filename
-    split_name = filename.split('/')
-    if(os.path.exists(cached_filename)):
-        return Config.CACHE_DIR + "/" + split_name[0] + "/01.jpg"
-    else:
-        raise CorgiNotFoundException("Corgi not found for emoji: {}"
-                                     .format(raw_emoji))
-
-
 def put_in_remote_cache(corgis):
     for i in corgis:
         corgi = corgis.get(i, None)
@@ -103,3 +76,6 @@ def get_from_remote_cache(raw_emoji):
     else:
         raise CorgiNotFoundException("Corgi not found for emoji: {}"
                                      .format(raw_emoji))
+
+def get_dir_for_corji(corji):
+
