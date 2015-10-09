b522dada02522822d8c6b309dc6d9d755f8666ab
Justin Duke <me@jmduke.com>
2015-10-09 13:36:33 -0700

    Integrate with Google Analytics.

--- corji/app.py
+++ corji/app.py
@@ -140,4 +140,5 @@ def fallback():
 @app.route("/", methods=['GET'])
 def about():
     """Much hype.  Very disruptive.  Such blurb."""
-    return render_template('html/about.html')
+    return render_template('html/about.html',
+                           google_analytics_id=settings.Config.GOOGLE_ANALYTICS_ID)
--- corji/settings.py
+++ corji/settings.py
@@ -2,8 +2,16 @@ import os
 
 
 class Config(object):
-    CACHE_DIR = os.getenv('CACHE_DIR', './.cache')
+    # General variables.
     DEBUG = os.getenv('DEBUG', False)
+
+    # Data variables.
+    CACHE_DIR = os.getenv('CACHE_DIR', './.cache')
+    SPREADSHEET_URL = os.getenv('SPREADSHEET_URL', '')
+
+    # Operational variables.
     LOG_NAME = os.getenv('LOG_NAME', 'corji.log')
     LOG_PATH = os.getenv('LOG_PATH', './.logs')
-    SPREADSHEET_URL = os.getenv('SPREADSHEET_URL', '')
+
+    # Frontend variables.
+    GOOGLE_ANALYTICS_ID = os.getenv('GOOGLE_ANALYTICS_ID', '')
