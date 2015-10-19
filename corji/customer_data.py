573694d271091c0253fea108b007f44f558df6df
Justin Duke <dukerson@gmail.com>
2015-10-19 22:22:27 +0100

    Sanitize phone numbers before loading into dynamo.

--- corji/customer_data.py
+++ corji/customer_data.py
@@ -7,10 +7,14 @@ dynamo_client = boto3.client("dynamodb", region_name="us-west-1")
 TABLE_NAME = Config.AWS_S3_CACHE_BUCKET_NAME
 
 
+def sanitize_phone_number(phone_number):
+    return phone_number.strip("+")
+
+
 def get(phone_number):
     response = dynamo_client.get_item(
         TableName=TABLE_NAME,
-        Key={'phone_number': {'S': phone_number}}
+        Key={'phone_number': {'S': sanitize_phone_number(phone_number)}}
     )
     if 'Item' in response:
         return response['Item']
@@ -29,7 +33,7 @@ def increment_consumptions(phone_number, consumptions):
     dynamo_client.update_item(
         TableName=TABLE_NAME,
         Key={
-            'phone_number': {"S": phone_number}
+            'phone_number': {"S": sanitize_phone_number(phone_number)}
         },
         AttributeUpdates={
             "consumptions": {
@@ -44,7 +48,7 @@ def decrement_consumptions(phone_number, consumptions=1):
     dynamo_client.update_item(
         TableName=TABLE_NAME,
         Key={
-            'phone_number': {"S": phone_number}
+            'phone_number': {"S": sanitize_phone_number(phone_number)}
         },
         AttributeUpdates={
             "consumptions": {
@@ -57,7 +61,7 @@ def decrement_consumptions(phone_number, consumptions=1):
 
 def new(phone_number):
     item = {
-        'phone_number': {'S': phone_number},
+        'phone_number': {'S': sanitize_phone_number(phone_number)},
         'consumptions': {'N': '19'}
     }
     put(item)
