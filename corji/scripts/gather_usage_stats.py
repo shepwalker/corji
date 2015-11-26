010b334f2632735696b7f0ef6a77bc5f920252fe
Shepherd Walker <shep.walker@gmail.com>
2015-11-26 15:54:25 -0500

    address initial easy feedback

--- corji/scripts/gather_usage_stats.py
+++ corji/scripts/gather_usage_stats.py
@@ -9,9 +9,5 @@ client = TwilioRestClient(Config.TWILIO_ACCOUNT_SID, Config.TWILIO_AUTH_TOKEN)
 if __name__ == "__main__":
 	with open("./.usage/test1.csv", "w+", newline = '') as csvfile:
 		csvwriter = csv.writer(csvfile)
-		t = 0
-		for message in client.messages.iter(page_size=1000): #Note, user .iter() in the future.
-			if(t>300000):
-				break
+		for message in client.messages.iter(page_size=1000): 
 			csvwriter.writerow([message.from_, message.to, message.body, message.date_sent, message.num_media, message.status, message.direction, message.error_message or ""])
-			t += 1
