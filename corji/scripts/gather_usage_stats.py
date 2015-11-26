f79e6fe83c458cb5833dac686b19ea05e134928d
Shepherd Walker <shep.walker@gmail.com>
2015-11-26 16:03:39 -0500

    tabs to spaces. blerg

--- corji/scripts/gather_usage_stats.py
+++ corji/scripts/gather_usage_stats.py
@@ -7,12 +7,12 @@ from corji.settings import Config
 
 client = TwilioRestClient(Config.TWILIO_ACCOUNT_SID, Config.TWILIO_AUTH_TOKEN)
 if __name__ == "__main__":
-	"""
-	Expects TWILIO_AUTH_TOKEN and TWILIO_ACCOUNT_SID to be filled in with 
-	appropriate values.
-	Gathers ALL twilio message logs and dumps them itno a CSV.  
-	"""
-	with open("./.usage/test1.csv", "w+", newline = '') as csvfile:
-		csvwriter = csv.writer(csvfile)
-		for message in client.messages.iter(page_size=1000): 
-			csvwriter.writerow([message.from_, message.to, message.body, message.date_sent, message.num_media, message.status, message.direction, message.error_message or ""])
+    """
+    Expects TWILIO_AUTH_TOKEN and TWILIO_ACCOUNT_SID to be filled in with 
+    appropriate values.
+    Gathers ALL twilio message logs and dumps them itno a CSV.  
+    """
+    with open("./.usage/test1.csv", "w+", newline = '') as csvfile:
+        csvwriter = csv.writer(csvfile)
+        for message in client.messages.iter(page_size=1000): 
+            csvwriter.writerow([message.from_, message.to, message.body, message.date_sent, message.num_media, message.status, message.direction, message.error_message or ""])
