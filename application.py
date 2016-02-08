248732bdd3dbb1d3c31cda184cb068e86d409187
Justin Duke <dukerson@gmail.com>
2016-02-07 16:17:02 -0800

    ugh

--- application.py
+++ application.py
@@ -5,5 +5,5 @@ import corji.settings as settings
 application = app
 
 if __name__ == '__main__':
-    attach_rest_api(application)
+    attach_rest_api(applicaation)
     application.run(port=settings.Config.PORT)
